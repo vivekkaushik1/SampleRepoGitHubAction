59f02b50147ee0a0a611badef60dbe547981ad4a
vivekkaushik1 <vivekit388@gmail.com>
2022-09-21 17:27:45 +0530

    Update secondTest.java

--- src/test/java/com/test/secondTest.java
+++ src/test/java/com/test/secondTest.java
@@ -19,7 +19,7 @@ public class secondTest {
 
 		Assert.assertTrue(true, "test is passed");
 	}
-/*	@Test
+	@Test
 	public void test3() {
 
 		for (int i = 0; i < 3; i++)
@@ -172,7 +172,817 @@ public class secondTest {
 			System.out.print("test summaries....");
 
 		Assert.assertTrue(true, "test is passed");
-	}*/	
+	}
+	@Test
+	public void test_2() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_4() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_6() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_5() {
+	
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_7() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_8() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_9() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_10() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_13() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_11() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_12() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_14() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_15() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_16() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_17() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_18() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_19() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_20() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_2() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_3() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_1_4() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_6() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_5() {
+	
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_7() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_8() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_9() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_10() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_1() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_13() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_11() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_12() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_14() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_15() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_16() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_1_17() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_1_18() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_1_19() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_1_20() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_2() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_3() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_2_4() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_6() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_5() {
+	
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_7() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_8() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_9() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_10() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_1() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_13() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_11() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_12() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_14() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_15() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_16() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_2_17() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_2_18() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_2_19() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_2_20() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_2() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_3() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_3_4() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_6() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_5() {
+	
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_7() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_8() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_9() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_10() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_1() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_13() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_11() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_12() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_14() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_15() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_16() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_3_17() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_3_18() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_3_19() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_3_20() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_2() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_3() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_4_4() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_6() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_5() {
+	
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_7() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_8() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_9() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_10() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_1() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_13() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_11() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_12() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_14() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_15() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_16() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+
+	@Test
+	public void test_4_17() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_4_18() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
+	@Test
+	public void test_4_19() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}	
+	@Test
+	public void test_4_20() {
+
+		for (int i = 0; i < 3; i++)
+			System.out.print("test summaries....");
+
+		Assert.assertTrue(true, "test is passed");
+	}
 	@Test
 	public void testskipped() {
 		throw new SkipException("skipped");
